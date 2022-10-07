19249e34f9e45852305e65b49a39f030dd0138db
renersullivan1 <renersullivan1@gmail.com>
2022-10-07 15:52:11 -0300

    incluindo css no toolbar

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
+import { StatusBar, Style } from '@capacitor/status-bar';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor() {
+    StatusBar.setOverlaysWebView({ overlay: true });
 
-  constructor() {}
+    const setStatusBarStyleDark = async () => {
+      await StatusBar.setStyle({ style: Style.Dark });
+    };
 
+    const setStatusBarStyleLight = async () => {
+      await StatusBar.setStyle({ style: Style.Light });
+    };
+
+    const hideStatusBar = async () => {
+      await StatusBar.hide();
+    };
+
+    const showStatusBar = async () => {
+      await StatusBar.show();
+    };
+  }
 }
