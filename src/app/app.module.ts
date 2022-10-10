03e57fb5eee506a935e02f92d882c6bdcaeba18f
renersullivan1 <renersullivan1@gmail.com>
2022-10-10 09:07:31 -0300

    criando componente btn-arounded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BtnRoundedComponent } from './components/btn-rounded/btn-rounded.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BtnRoundedComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
