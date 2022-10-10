818febd2d1628a2cec4d573d7eb43c2b8bad2c74
renersullivan1 <renersullivan1@gmail.com>
2022-10-10 10:37:18 -0300

    Adicionando botoẽs e scroll

---
+++ src/app/components/btn-rounded/btn-rounded.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-rounded',
+  templateUrl: './btn-rounded.component.html',
+  styleUrls: ['./btn-rounded.component.scss'],
+})
+export class BtnRoundedComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
