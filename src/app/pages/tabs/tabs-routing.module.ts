3ba601f04ca39c702295bbc104fdbf69733b6654
renersullivan1 <renersullivan1@gmail.com>
2022-10-13 15:31:42 -0300

    adicionando tab bar

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../../pages/home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'carteira',
+        loadChildren: () =>
+          import('../../pages/home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
