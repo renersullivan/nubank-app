eae906ae6a51ae3c476c228f1ac31a25ea836d18
renersullivan1 <renersullivan1@gmail.com>
2022-10-17 10:02:43 -0300

    introduzindo divs e icons page investimentos

---
+++ src/app/pages/investimentos/investimentos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-investimentos',
+  templateUrl: './investimentos.page.html',
+  styleUrls: ['./investimentos.page.scss'],
+})
+export class InvestimentosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
