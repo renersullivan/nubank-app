bfff9fa2927dfc97896668325f4c280588a3f2ab
renersullivan1 <renersullivan1@gmail.com>
2022-10-18 16:58:30 -0300

    fazendo integracao e correções com back end

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,7 +3,7 @@ import { ActivationStart, Router } from '@angular/router';
 
 import { HttpClient } from '@angular/common/http';
 
-import { homeResponse, investResponse } from 'src/app/model/home.model';
+import { homeResponse } from 'src/app/model/home.model';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +12,10 @@ import { homeResponse, investResponse } from 'src/app/model/home.model';
 })
 export class HomePage implements OnInit {
   resposta: homeResponse;
-  investimento: investResponse;
 
   constructor(private router: Router, private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    console.log('chamou aqui');
     this.httpClient
       .get<homeResponse>('http://localhost:3000/home')
       .subscribe((sacola) => {
