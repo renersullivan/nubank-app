be606f6541059a26b72dad9e9a6855fbc3753507
renersullivan1 <renersullivan1@gmail.com>
2022-10-21 11:58:06 -0300

    adicionando modal para so botoes are pix

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,6 +4,8 @@ import { ActivationStart, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 import { homeResponse } from 'src/app/model/home.model';
+import { ModalController } from '@ionic/angular';
+import { ModalMessageComponent } from '../../components/modal-message/modal-message.component';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +14,13 @@ import { homeResponse } from 'src/app/model/home.model';
 })
 export class HomePage implements OnInit {
   resposta: homeResponse;
+  modelData: any;
 
-  constructor(private router: Router, private httpClient: HttpClient) {}
+  constructor(
+    private router: Router,
+    private httpClient: HttpClient,
+    public modalController: ModalController
+  ) {}
 
   hideValue: boolean = true;
 
@@ -55,5 +62,22 @@ export class HomePage implements OnInit {
 
   navigate() {
     this.router.navigate(['/home']);
+  }
+
+  async openIonModal(param) {
+    const modal = await this.modalController.create({
+      component: ModalMessageComponent,
+      componentProps: {
+        model_title: param,
+      },
+    });
+
+    modal.onDidDismiss().then((modelData) => {
+      if (modelData !== null) {
+        this.modelData = modelData.data;
+        console.log('Modal Data : ' + modelData.data);
+      }
+    });
+    return await modal.present();
   }
 }
